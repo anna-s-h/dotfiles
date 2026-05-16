61b7e7484c838e88413938b1309fe2644efa7af0
anna-s-h <annasonohana@gmail.com>
2026-05-16 13:05:31 -0400

    gor boot 10

--- hosts/gor/hardware-configuration.nix
+++ hosts/gor/hardware-configuration.nix
@@ -19,8 +19,9 @@
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1dd0693a-de30-4076-b425-5dc28c17afd1";
+    { device = "/6351-8343";
       fsType = "vfat";
+      neededForBoot = true;
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
